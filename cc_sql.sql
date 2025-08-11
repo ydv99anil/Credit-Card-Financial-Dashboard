d047b50fa7bdf70551f64155f0f84c38cd8f145c
Anil Yadav <ydv99anil@gmail.com>
2025-08-11 10:07:58 +0530

    Add files from my PC

---
+++ cc_sql.sql
@@ -0,0 +1,73 @@
+CREATE DATABASE credit_card_database ;
+USE credit_card_database;
+
+CREATE TABLE cc_detail (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+
+CREATE TABLE cust_detail (
+    Client_Num INT,
+    Customer_Age INT,
+    Gender VARCHAR(5),
+    Dependent_Count INT,
+    Education_Level VARCHAR(50),
+    Marital_Status VARCHAR(20),
+    State_cd VARCHAR(50),
+    Zipcode VARCHAR(20),
+    Car_Owner VARCHAR(5),
+    House_Owner VARCHAR(5),
+    Personal_Loan VARCHAR(5),
+    Contact VARCHAR(50),
+    Customer_Job VARCHAR(50),
+    Income INT,
+    Cust_Satisfaction_Score INT
+);
+
+
+
+SELECT * FROM cc_detail;
+
+SELECT * FROM cust_detail;
+
+
+SET GLOBAL local_infile = 1;
+
+SHOW GLOBAL VARIABLES LIKE 'local_infile';
+
+
+
+
+LOAD DATA LOCAL INFILE 'D:\\DK\\CODE FILES\\Power BI Project\\Credit Card Report\\cc_add.csv'
+INTO TABLE cc_detail
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+LOAD DATA LOCAL INFILE 'D:\\DK\\CODE FILES\\Power BI Project\\Credit Card Report\\cust_add.csv'
+INTO TABLE cust_detail
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
